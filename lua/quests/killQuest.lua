b41a8cd4e87c8370872bbad0d8aefd93fe5db35f
Marvin Lübbers <maffinlp@yahoo.com>
2025-02-03 02:37:39 +0100

    Updated a string

--- lua/quests/killQuest.lua
+++ lua/quests/killQuest.lua
@@ -4,7 +4,7 @@ KillQuest.__index = KillQuest
 
 function KillQuest:new(args)
     if not args[2] || args[2] == 0 || not args[3] || not args[4] then
-        PrintPink("Usage: AddQuest <QuestType> [requiredKills] [roleToBeKilled] [roleForKiller]")
+        PrintPink("Usage: AddQuest KillQuest [requiredKills] [roleToBeKilled] [roleForKiller]")
         PrintPink("Innocent: " .. ROLE_INNOCENT)
         PrintPink("Traitor: " .. ROLE_TRAITOR)
         PrintPink("Detective: " .. ROLE_DETECTIVE)
