2ea758653c1e3dc640378bf8aea21dcff49c26bc
Marvin Lübbers <maffinlp@yahoo.com>
2025-02-16 09:12:38 +0100

    Fixed quest menu opening for everyone lmfao

--- lua/cl/chatCommands.lua
+++ lua/cl/chatCommands.lua
@@ -1,7 +1,9 @@
 hook.Add("OnPlayerChat", "QueueChatCommand", function(ply, text, teamChat, isDead)
     local input = string.lower(text)
     if input == "!quest" or input == "!quests"  then
-        OpenQuestMenu()
+        if ply == LocalPlayer() then
+            OpenQuestMenu()
+        end
         return true
     end        
 end)
